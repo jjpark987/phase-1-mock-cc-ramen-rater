fb841d3bad76e4be878a61711546eb95e7786921
Jason Park <jjpark987@gmail.com>
2022-11-30 10:33:21 -0800

    commit tree c8e964c593c4ab81909025892e08d427bd9598cd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('#ramen-menu')
+    const image = document.querySelector('img.detail-image')
+    const name = document.querySelector('h2.name')
+    const restaurant = document.querySelector('h3.restaurant')
+    const rating = document.querySelector('#rating-display')
+    const comment = document.querySelector('#comment-display')
+    const form = document.querySelector('#new-ramen')
+
+    fetch('http://localhost:3000/ramens')
+    .then(response => response.json())
+    .then(data => {
+        for(const ramen of data) {
+            addRamen(ramen)
+        }
+    })
+
+    form.addEventListener('submit', event => {
+        event.preventDefault()
+        
+        const newRamen = {
+            name: event.target[0].value,
+            restaurant: event.target[1].value,
+            image: event.target[2].value,
+            rating: event.target[3].value,
+            comment: event.target[4].value
+        }
+
+        addRamen(newRamen)
+    })
+
+    function addRamen(ramenObj) {
+        const menuImage = document.createElement('img')
+        menuImage.src = ramenObj.image
+        menuImage.addEventListener('click', () => {
+            image.src = ramenObj.image
+            name.textContent = ramenObj.name
+            restaurant.textContent = ramenObj.restaurant
+            rating.textContent = ramenObj.rating
+            comment.textContent = ramenObj.comment
+        })
+        menu.append(menuImage)
+    }
+})
+
